3ad9cb476fcf3500b3f3335b3dd749ed9a247788
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-22 21:12:54 +0530

    Sample commit

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
 });
 
 server.listen(3000);
