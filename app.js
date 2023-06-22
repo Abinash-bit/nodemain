9efbfdc7ce155adc44da7eaaea459e9a7715d60f
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-22 21:32:17 +0530

    Sample commit

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     if (url === '/'){
       res.write('<html>');
-      res.write('<head><title>Enter message</title><head>');  
+      res.write('<head><title>Enter message</title><head>');
+      res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');  
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
