67efcde4829a5d94aa723a158ecdc65204fdd67e
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-24 00:15:53 +0530

    Sample commit

---
+++ routes.js
@@ -0,0 +1,28 @@
+if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+}
+if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt', message);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    });
+}
+
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>My first Page</title></head>');
+res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+res.write('</html>');
+res.end();
