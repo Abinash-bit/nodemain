0a2bfc6f17e1387d32869097e03f7d08b49319ee
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-24 00:22:47 +0530

    Sample commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+
 });
 
 server.listen(3000);
--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 const requestHandler = (req, res) => {
+
+    const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
