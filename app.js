c5ba640f2b730101184f3f308fdf338d2c4261c0
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-24 23:49:31 +0530

    Sample commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
