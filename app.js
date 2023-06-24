13b5bba7b31a56b1de03631012e5d18f6a2a2c51
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-24 20:56:48 +0530

    Sample commit

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 
 const express = require('express');
 
+
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-products', (req, res, next) => {
-  console.log('In the middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
 
 app.use('/',(req, res, next) => {
   
