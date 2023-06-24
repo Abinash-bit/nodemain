7dd8f8a0c74e122524741ee1be23c67cca10f2dc
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-25 02:49:04 +0530

    Sample commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
-
 router.get('/add-products', (req, res, next) => {
-    
     res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
   });
-  
   router.post('/products', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
   });
-
 module.exports = router;
