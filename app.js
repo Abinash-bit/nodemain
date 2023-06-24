6246520ab71485ab49741aeb28e5fe84410eff05
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-24 21:51:36 +0530

    Sample commit

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use('/',(req, res, next) => {
   
