cf4e989c22e65ee0b6a296a6e76d3b417fde1c4a
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-24 23:41:42 +0530

    Sample commit

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-products', (req, res, next) => {
-    console.log('In the middleware!');
+    
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
   });
   
