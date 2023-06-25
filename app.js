581c395227f12a3a6e2e7ace5e35aa0d8b8f07ba
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-25 12:03:33 +0530

    Sample commit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+
+
 const bodyParser = require('body-parser');
+
+
 const app = express();
+
+
 const adminRoutes = require('./routes/admin.js');
+
+
 const shopRoutes = require('./routes/shop.js');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.use(adminRoutes);
+
+
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found</h1>');
