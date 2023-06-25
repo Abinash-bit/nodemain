caf54f164774bc3b5acb939db48a257ad9dabfe3
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-25 23:04:38 +0530

    Sample commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(adminRoutes);
+app.use(adminData.routes);
 
 
 app.use(shopRoutes);
