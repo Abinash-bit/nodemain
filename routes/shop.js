a34a8f3805b573637275ad115d78afe906943460
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-25 23:47:01 +0530

    Sample commit

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,8 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {  
+router.get('/',(req, res, next) => { 
+  console.log(adminData.products); 
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
