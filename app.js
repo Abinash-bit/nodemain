bf73002f97ad7a21fbafc4ad6e6d3764d5a1c225
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-25 21:11:16 +0530

    Sample commit

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', '404.html'));
 });
 app.listen(3000);
