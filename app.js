78813240acba6a3a0277598ebac327224dbcebb1
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-27 14:24:03 +0530

    Sample commit

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use(adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 app.listen(3000);
