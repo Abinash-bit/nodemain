467b5a83122d8d796b98a5fdc263939d5890e9ca
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-27 14:41:42 +0530

    Sample commrot

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 
 
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 
 const app = express();
-
-app.set('view engine', 'pug');
+app.engine('handlebars', expressHbs());
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
 
