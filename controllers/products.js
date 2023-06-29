3ce57cc833c9ca44a0cf8b3be59e099837930bc9
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-29 23:36:50 +0530

    Sample commit

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const products = [];
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { pageTitle: 'Add Product', path: '/add-products' });
   };
-
+ 
   exports.postAddProduct =  (req, res, next) => {
     products.push({title: req.body.title});
     res.redirect('/');
