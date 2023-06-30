4f3babf894d8159ac0cc9ed00e08bc87b131ea37
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-30 23:42:07 +0530

    Sample commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,2 @@
-const path = require('path');
-const express = require('express');
 
-const productsController = require('../controllers/products');
-
-const router = express.Router();
-
-
-
-
-router.get('/add-products', productsController.getAddProduct);
-
-router.post('/products', productsController.postAddProduct);
-
-module.exports = router;
 
