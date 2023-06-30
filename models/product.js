a8c0449ac4c82ea245a1cc2afe1145228dfd0a06
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-01 00:17:03 +0530

    Sample commit

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const products = [];
+module.exports = class Product {
+    constructor(title) {
+        this.title = t;
+    }
+    save() {
+        products.push(this);
+
+    }
+    fetchAll() {
+        return this.products;
+    }
+}
