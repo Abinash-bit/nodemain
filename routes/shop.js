ff537afd28d867133a801c32a5d47ac9ebbd8ecc
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-01 21:43:30 +0530

    Sample commit

--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,8 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/cart');
+router.get('/cart', shopController.getCart);
 
-router.get('/checkout');
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
