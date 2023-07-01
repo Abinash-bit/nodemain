6b969a14522720223c7dac1f8dc42f031f021eb2
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-01 21:40:18 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -30,5 +30,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout',{
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+}
+
 
   
