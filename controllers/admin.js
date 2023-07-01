7f5dc66f8a3b9855c897ca2bbcc749807c9fedef
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-01 20:12:31 +0530

    Sample commit

---
+++ controllers/admin.js
@@ -0,0 +1,15 @@
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+  };
+  
+  exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+  };
