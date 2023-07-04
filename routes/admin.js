5ee8ab99b78104f208fa9c9a74d6226206885fc2
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-04 23:25:13 +0530

    Sample commit

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-router.get('/products');
+router.get('/products', adminController.getAddProduct);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
