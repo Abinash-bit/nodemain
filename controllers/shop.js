133a4c360c2aae74da084c93937592f11c0a7e82
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-18 21:18:42 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,7 +15,7 @@ exports.getProduct = (req, res, next) => {
   Product.findById(prodId, product => {
     console.log(product);
   })
-  res.redirect('/');
+  
 };
 
 exports.getIndex = (req, res, next) => {
