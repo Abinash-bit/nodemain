653032e0f68ef70d0aad7a846b3a59cda9c37d77
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-18 23:31:56 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -35,6 +35,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
