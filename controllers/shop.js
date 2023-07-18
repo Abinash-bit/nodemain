f03a4ddb1032af1cc62c19d1edcb1e0dd3501b2f
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-18 21:20:21 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,7 +13,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    console.log(product);
+    res.render('shop/product-detail')
   })
   
 };
