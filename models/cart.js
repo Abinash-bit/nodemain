0b72293bc6dc03ea468bd1f40ca40702ac54932f
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-19 22:36:52 +0530

    Sample commit

--- models/cart.js
+++ models/cart.js
@@ -23,6 +23,8 @@ module.exports = class Cart {
             if(existingProduct) {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
 
             } else {
                 updatedProduct = { id: id, qty: 1};
