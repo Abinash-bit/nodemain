de81cd1719b743e1dfd64151e1fbcc83dd251233
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-19 23:53:09 +0530

    sample commit

--- models/cart.js
+++ models/cart.js
@@ -2,47 +2,46 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'cart.json'
-  );
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
 
 module.exports = class Cart {
-    static addProduct(id) {
-        // Fetch the previous cart
-        fs.readFile(p, (err, fileContent) =>{
-            let cart = {products: [], totalPrice: 0}
-            if(!err) {
-                cart = JSON.parse(fileContent);
-            }
-            // Analyze the cart => Find existing product
-            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-            const existingProduct = cart.products[existingProductIndex];
-            let updatedProduct;
-        // Add new product/ increase
-            if(existingProduct) {
-                updatedProduct = { ...existingProduct };
-                updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [...cart.products];
-                cart.products[existingProductIndex] = updatedProduct;
+  static addProduct(id) {
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex((prod) => prod.id === id);
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      
+      // Add new product/increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      
+      // Retrieve the price of the product (hypothetical function)
+      const productPrice = getProductPrice(id);
 
-            } else {
-                updatedProduct = { id: id, qty: 1};
-                cart.products = [ ...cart.products, updatedProduct];
-            }
-
-            cart.totalPrice = cart.totalPrice + productPrice;
-            fs.writeFile(p, JSON.stringify(cart), (err) => {
-                console.log(err);
-
-            })
-            
-
-        });
-        
-
-        // Add new product/ increase
-
-    }
-
-}
+      cart.totalPrice = cart.totalPrice + productPrice;
+      
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+};
