e22438c39f0a815838394fa8bb56a4dbf1b5fea4
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-19 20:14:11 +0530

    Sample commit

--- models/cart.js
+++ models/cart.js
@@ -4,7 +4,7 @@ const path = require('path');
 const p = path.join(
     path.dirname(process.mainModule.filename),
     'data',
-    'products.json'
+    'cart.json'
   );
 
 module.exports = class Cart {
