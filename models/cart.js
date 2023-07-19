25bcf22f4b3c42c63de6c62376f0706fd38c904b
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-19 22:45:23 +0530

    Sample commit

--- models/cart.js
+++ models/cart.js
@@ -33,6 +33,7 @@ module.exports = class Cart {
 
             cart.totalPrice = cart.totalPrice + productPrice;
             fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
 
             })
             
