18c28f4375cf3bed4c498d078e6673374209a4c2
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-19 20:09:14 +0530

    Sampel commit

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,8 @@
 module.exports = class Cart {
     static addProduct(id) {
         // Fetch the previous cart
+        // Analyze the cart => Find existing product
+        // Add new product/ increase
 
     }
 
