99c3cd26c3eb3a4765fc8936ff3f8138832275f2
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-20 00:00:10 +0530

    Sample commit

--- models/cart.js
+++ models/cart.js
@@ -8,20 +8,20 @@ const p = path.join(
 );
 
 module.exports = class Cart {
-  static addProduct(id) {
+  static addProduct(id, productPrice) {
     // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-      
       // Analyze the cart => Find existing product
-      const existingProductIndex = cart.products.findIndex((prod) => prod.id === id);
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      
-      // Add new product/increase quantity
+      // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
@@ -31,16 +31,9 @@ module.exports = class Cart {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      
-      // Retrieve the price of the product (hypothetical function)
-      const productPrice = getProductPrice(id);
-
-      cart.totalPrice = cart.totalPrice + productPrice;
-      
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        if (err) {
-          console.log(err);
-        }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
       });
     });
   }
