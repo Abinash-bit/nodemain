08100c7f8815765f83ac578704440d26fc756a7f
Abinash-bit <abinashmahapatra41@gmail.com>
2023-07-19 20:13:11 +0530

    Sample commit

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+  );
+
 module.exports = class Cart {
     static addProduct(id) {
         // Fetch the previous cart
