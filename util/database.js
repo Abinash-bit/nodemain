003ae5f1551fdefb68ae3f0696e1f838e7ec2cf7
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 01:55:43 +0530

    Console log connected

--- util/database.js
+++ util/database.js
@@ -1,4 +1,14 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority')
+MongoClient.connect(
+    'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority'
+    )
+
+       .then(result => {
+        console.log('Connected');
+       })
+
+       .catch(err => {
+        console.log(err);
+       });
