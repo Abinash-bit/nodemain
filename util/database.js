d8cb445fdeaae72b7f7676d1a3b71e31f321869e
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 01:44:34 +0530

    connecting to database

---
+++ util/database.js
@@ -0,0 +1,4 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect('mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority')
