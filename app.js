9bb93e6ce6784d3e7bf953d4a9a30b39a524c310
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 02:11:29 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database')
 
 const app = express();
 
@@ -20,5 +21,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+mongoConnect()
 
 app.listen(3000);
