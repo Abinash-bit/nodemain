8e40e21096190aa6d3513513327c462129e8e3a3
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 02:00:39 +0530

    Call back mongoconnect

--- util/database.js
+++ util/database.js
@@ -1,14 +1,17 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(
-    'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority'
-    )
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority'
+        )
+    
+           .then(result => {
+            console.log('Connected');
+           })
+    
+           .catch(err => {
+            console.log(err);
+           });
+}
 
-       .then(result => {
-        console.log('Connected');
-       })
-
-       .catch(err => {
-        console.log(err);
-       });
