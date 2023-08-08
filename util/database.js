8c7901b60742be1b987c6443bc2645fe6e2ade8a
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 23:51:30 +0530

    Sample commit

--- util/database.js
+++ util/database.js
@@ -6,17 +6,16 @@ let _db;
 const mongoConnect = (callback) => {
   MongoClient.connect(
     'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/shop?retryWrites=true&w=majority'
-
-)
-  .then(client => {
-    console.log('Connected!');
-    _db = client.db();
-    callback(client);
-  })
-  .catch(err => {
-    console.log(err);
-    throw err;
-  });
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
 const getDb = () => {
@@ -24,10 +23,7 @@ const getDb = () => {
     return _db;
   }
   throw 'No database found!';
-
 };
-
-module.exports = mongoConnect;
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
