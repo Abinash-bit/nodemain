31d2bf6b8f9d5499d3bb556dc4cac638e2bcf650
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 02:42:40 +0530

    Sample commit

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 
@@ -43,7 +44,7 @@ class Product {
     const db = getDb();
     return db
     .collection('products')
-    .find({_id: prodId})
+    .find({ _id: new mongodb.ObjectId (prodId)})
     .next()
     .then(product => {
       console.log(product);
