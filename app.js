766c825ae324132870154b9e88c82774aad3544c
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 21:11:14 +0530

    Sample commit

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
