7366c491737cc1760c88f901ce029a69c357a8a6
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 23:01:02 +0530

    Sample commit

--- util/database.js
+++ util/database.js
@@ -5,11 +5,12 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/shop?retryWrites=true&w=majority'
 
 )
   .then(client => {
     console.log('Connected!');
+    _db = client.db();
     callback(client);
   })
   .catch(err => {
