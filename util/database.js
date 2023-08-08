4a451a28d360c4cad1ea32c8b303ad051e3a5047
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 20:59:51 +0530

    Sample commit

---
+++ util/database.js
@@ -0,0 +1,13 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(
+    'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/?retryWrites=true&w=majority'
+
+)
+  .then(result => {
+    console.log('Connected!');
+  })
+  .catch(err => {
+    console.log(err);
+  })
