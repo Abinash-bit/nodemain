aafcd93b310573849d5cc6de13ce0347961aa895
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-08 18:27:31 +0530

    Sample commit

--- util/database.js
+++ util/database.js
@@ -1,29 +1,8 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const Sequelize = require('sequelize');
 
-let _db;
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-const mongoConnect = callback => {
-  MongoClient.connect(
-    'mongodb+srv://abinash-bit:abinash@cluster0.orm6zuo.mongodb.net/shop?retryWrites=true&w=majority'
-  )
-    .then(client => {
-      console.log('Connected!');
-      _db = client.db();
-      callback();
-    })
-    .catch(err => {
-      console.log(err);
-      throw err;
-    });
-};
-
-const getDb = () => {
-  if (_db) {
-    return _db;
-  }
-  throw 'No database found!';
-};
-
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
+module.exports = sequelize;
