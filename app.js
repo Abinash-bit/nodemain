9e833ba82537978fb6b179b6ef4aeeaa66b2b34c
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 18:25:32 +0530

    Sample commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
