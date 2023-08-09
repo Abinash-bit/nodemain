9acae1c6d876a7264270c24f2afe0b3899669c67
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 15:45:07 +0530

    Sample commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,8 +17,7 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(title, price, description, imageUrl);
-
-    product
+  product
     .save()
     .then(result => {
       // console.log(result);
@@ -37,7 +36,8 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(product => { // Change 'products' to 'product'
+    // Product.findById(prodId)
+    .then(product => {
       if (!product) {
         return res.redirect('/');
       }
@@ -51,7 +51,6 @@ exports.getEditProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
@@ -59,33 +58,21 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  // Assuming your Product model has a findById method
-  Product.findById(prodId)
-    .then(productData => {
-      const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, new ObjectId(prodID))
-
-
-      if (!product) {
-        return res.redirect('/admin/products');
-      }
-
-      // Update the product properties
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      product.description = updatedDesc;
-
-      // Save the updated product
-      return product.save();
-    })
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    new ObjectId(prodId)
+  );
+  product
+    .save()
     .then(result => {
-      console.log('UPDATED PRODUCT');
+      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
 };
-
-
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
@@ -98,7 +85,6 @@ exports.getProducts = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
-
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const prodId = req.body.productId;
