d77fe1465aaf45ad938b6ed2048ae6549c60539b
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 14:06:34 +0530

    Sample commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -56,11 +56,8 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+    .then(productData => {
+
       return product.save();
     })
     .then(result => {
