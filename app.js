103af185e49f70f1eb1cd462cc2b7953ffbcb970
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 19:31:53 +0530

    Sample commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById(1)
+  User.findById('64d39b55165adcf684341df9')
     .then(user => {
       req.user = user;
       next();
