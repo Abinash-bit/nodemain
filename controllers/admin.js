8a724e6007e2e98ac9d99020d8062f71bdf6b0f5
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 16:25:07 +0530

    Sample commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
-const mongodb = require('mongodb');
+
 const Product = require('../models/product');
 
-const ObjectId = mongodb.ObjectId;
+
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
