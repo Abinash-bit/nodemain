e02923307fc7626edc0b77a525498a1b2b079572
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-09 18:16:20 +0530

    Sample commit

--- models/user.js
+++ models/user.js
@@ -1,13 +1,20 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
 class User {
   constructor(username, email) {
     this.name = username;
     this.email = email;
   }
   save(){
+    const db = getDb();
+    return db.collection('users').insertOne(this);
 
   }
   static findId(userId){
-    
+    const db = getDb();
+    return db.collection('users').find({_id: })
+
   }
 }
 
