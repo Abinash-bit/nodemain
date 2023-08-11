e0afa09cdc5dcbe66b50ddc0255b4d7a7bf45cf5
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-11 16:33:57 +0530

    Sample commit

--- models/user.js
+++ models/user.js
@@ -19,7 +19,9 @@ class User {
     // const cartProduct = this.cart.items.findIndex(cp => {
     //   return cp._id === product._id;
     // });
-    const updatedCart = {}
+    product.quantity = 1;
+    const updatedCart = {items: [{...product, quantity: 1}]};
+    const db = getDb();
 
   }
 
