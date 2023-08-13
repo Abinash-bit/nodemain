c17810dd0fb212b2fdc1075a973f70aafa4e8c19
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-13 20:16:46 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -66,8 +66,10 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
+  Product.findById(prodId)
+  .then(product => {
     return req.user.addToCart(product);
+    res.redirect('/cart');
   })
   .then(result => {
     console.log(result);
