eb857992af6e019e222e269aaa5909c7e6728892
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-13 21:26:47 +0530

    Sample commit

--- models/user.js
+++ models/user.js
@@ -80,6 +80,14 @@ class User {
 
   addOrder() {
     const db = getDb();
+    const order = {
+      items: this.cart.items,
+      user: {
+        _id: new ObjectId(this._id),
+        name: this.name,
+        
+      }
+    }
     return db
     .collection('orders')
     .insertOne(this.cart)
