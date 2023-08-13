74c1e283e5d25c7182a48bb6f37da23b90aa3f83
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-13 20:46:23 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -80,14 +80,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .deleteItemFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
