fc07264b582ea7383ba16f7b4e7479ca8ecc9bb2
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-14 16:32:21 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,7 +38,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
