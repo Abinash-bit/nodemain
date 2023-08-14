7e81291d6244ad4e18a8cc84c6368b1a209230a7
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-14 15:59:37 +0530

    Ssample commi

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
