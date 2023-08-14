46a4092c169c7494fa41c9a3b598963bf72d6192
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-14 15:48:12 +0530

    Sample commit

--- models/product.js
+++ models/product.js
@@ -6,6 +6,14 @@ const productSchema = new Schema({
     title: {
         type: String,
         required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
     }
 });
 
