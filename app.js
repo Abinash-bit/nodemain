babd7567a4d183a5911fdd6f3ad331d38848bb40
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-14 15:39:01 +0530

    Sample commit

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://abinash-bit:Abinash@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb://abinash-bit:Abinash@ac-xuw2vgs-shard-00-00.orm6zuo.mongodb.net:27017,ac-xuw2vgs-shard-00-01.orm6zuo.mongodb.net:27017,ac-xuw2vgs-shard-00-02.orm6zuo.mongodb.net:27017/?ssl=true&replicaSet=atlas-rfsxph-shard-0&authSource=admin&retryWrites=true&w=majority'
   )
   .then(result => {
     app.listen(3000);
