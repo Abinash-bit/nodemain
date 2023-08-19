95212b1bd2bf6fdbcf4d13af8dbd246f520d9e7c
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-19 20:35:37 +0530

    Sample commit

--- models/order.js
+++ models/order.js
@@ -3,5 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const orderschema = new Schema({
+    products: { type: Object, required: true },
+    quantity: { type: Number, required: true }
 
 });
