bf4f1a40b474953e30598eb4225ceaceeafbbdff
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-20 01:50:42 +0530

    Sample commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -83,7 +83,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   req.user
   .populate('cart.items.productId')
-  
   .then(user => {
     const products = user.cart.items.map(i => {
       return {quantity: i.quantity, product: i.productId};
