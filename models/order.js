fc0d755621c50a57410ebc3fee8a168cac61cf91
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-21 23:08:05 +0530

    Sample commit

--- models/order.js
+++ models/order.js
@@ -1,30 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const orderschema = new Schema({
-    products: [
-        {
-            product: { type: Object, required: true },
-            quantity: { type: Number, required: true }
-
-
-        }
-
-
-    ],
-    user: {
-        name: {
-            type: String,
-            required: true
-        },
-        userId: {
-            type: Schema.Types.ObjectId,
-            required: true,
-            ref: 'User'
-        }
+const orderSchema = new Schema({
+  products: [
+    {
+      product: { type: Object, required: true },
+      quantity: { type: Number, required: true }
     }
-
+  ],
+  user: {
+    name: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
+  }
 });
 
-module.exports = mongoose.model('Order', orderschema);
+module.exports = mongoose.model('Order', orderSchema);
