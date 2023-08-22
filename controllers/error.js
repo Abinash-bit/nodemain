b4660d276e2c9908e272a843be0c390b42361c6b
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-22 19:33:14 +0530

    Sample commit

--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,7 @@
 exports.get404 = (req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
+    path: '/404',
+    isAuthenticated: req.isLoggedIn
+  });
 };
-
-// mongodb://abinash-bit:Abinash@ac-xuw2vgs-shard-00-00.orm6zuo.mongodb.net:27017,ac-xuw2vgs-shard-00-01.orm6zuo.mongodb.net:27017,ac-xuw2vgs-shard-00-02.orm6zuo.mongodb.net:27017/?ssl=true&replicaSet=atlas-rfsxph-shard-0&authSource=admin&retryWrites=true&w=majority
-// 64da49ce4a5473868b4b014e
