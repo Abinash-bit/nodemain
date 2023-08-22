252ce95c4b7beccec641c96a7a7165c80b130101
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-22 23:27:39 +0530

    Sample commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie'));
+  console.log(req.get('Cookie').split(';')[1].trim().split('=')[1]);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
