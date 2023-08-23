7c9db0555b578fcadbfe894f61a3537fec4afcad
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-23 22:55:19 +0530

    Sample commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,26 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-  // .get('Cookie')
-  // .split(';')[0]
-  // .trim()
-  // .split('=')[0];
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
+  User.findById('64da49ce4a5473868b4b014e')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
     res.redirect('/');
-  };
+  });
+};
