4a5b14cf345b3fa56ce3d2b2799d2c74c477f299
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-24 23:46:41 +0530

    Sample commit

--- app.js
+++ app.js
@@ -53,22 +53,8 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    MONGODB_URI
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Abinash Mahapatra',
-          email: 'abinashmahapatra41@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
