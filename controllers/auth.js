49ef0b8ae1373ae880ad69bbd1c0dda97f147c5b
Abinash-bit <abinashmahapatra41@gmail.com>
2023-08-27 15:08:12 +0530

    Sample commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -129,38 +129,38 @@ exports.getReset = (req, res, next) => {
   });
 };
 
-exports.postReset = (req, res, next) => {
-  crypto.randomBytes(32, (err, buffer) => {
-    if (err) {
-      console.log(err);
-      return res.redirect('/reset');
-    }
-    const token = buffer.toString('hex');
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          req.flash('error', 'No account with that email found.');
-          return res.redirect('/reset');
-        }
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 3600000;
-        return user.save();
-      })
-      .then(result => {
-        res.redirect('/');
-        transporter.sendMail({
-          to: req.body.email,
-          from: 'abinashmahapatra41@gmail.com',
-          subject: 'Password reset',
-          html: `
-            <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
-          `
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-};
+// exports.postReset = (req, res, next) => {
+//   crypto.randomBytes(32, (err, buffer) => {
+//     if (err) {
+//       console.log(err);
+//       return res.redirect('/reset');
+//     }
+//     const token = buffer.toString('hex');
+//     User.findOne({ email: req.body.email })
+//       .then(user => {
+//         if (!user) {
+//           req.flash('error', 'No account with that email found.');
+//           return res.redirect('/reset');
+//         }
+//         user.resetToken = token;
+//         user.resetTokenExpiration = Date.now() + 3600000;
+//         return user.save();
+//       })
+//       .then(result => {
+//         res.redirect('/');
+//         transporter.sendMail({
+//           to: req.body.email,
+//           from: 'abinashmahapatra41@gmail.com',
+//           subject: 'Password reset',
+//           html: `
+//             <p>You requested a password reset</p>
+//             <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+//           `
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   });
+// };
 //847A8A8D42E343B4492E66070F6CD91983E52E69CBD5B1792830EBD771BF205AC1936579E9DC42FCA7871B3C4F0942E2
