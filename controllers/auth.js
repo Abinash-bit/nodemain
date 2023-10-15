75e6662906e02eaccdda89330d4e523f36bdd95e
Abinash-bit <abinashmahapatra41@gmail.com>
2023-10-15 21:38:48 +0530

    Sample commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const User = require('../models/user');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
   auth: {
-    api_user
+    api_key: ''
   }
 }))
 
@@ -76,6 +76,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!<h1>'
+          }); 
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
