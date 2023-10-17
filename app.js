755363fe183291405056e13e1d71bedb186ad989
Abinash-bit <abinashmahapatra41@gmail.com>
2023-10-17 19:41:18 +0530

    Sample commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
+  'mongodb+srv://abinash-bit:Abinash@cluster0.orm6zuo.mongodb.net/test';
 
 const app = express();
 const store = new MongoDBStore({
